87fdd1796125eddb6bf876c2ec19a16af2c94d58
Carl Klemmer <ckone989@gmail.com>
2013-05-19 10:30:43 -0400

    Finished 3.6 Advanced up to sublime setup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let (:base_title) { "Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,8 +13,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', 
-    		:test => "Ruby on Rails Tutorial Sample App | Home")
+    	page.should have_selector('title', :test => "#{base_title} | Home")
     end
   end
 
@@ -25,8 +26,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	page.should have_selector('title', 
-    		:test => "Ruby on Rails Tutorial Sample App | Help")
+    	page.should have_selector('title', :test => "#{base_title} | Help")
     end
   end
 
@@ -39,8 +39,21 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	page.should have_selector('title', 
-    		:test => "Ruby on Rails Tutorial Sample App | About Us")
+    	page.should have_selector('title', :test => "#{base_title} | About Us")
+    end
+
+  end
+
+  describe "Contact" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :test => "#{base_title} | Contact")
     end
 
   end
